a08bba78ad108de1da0955942cc5c9e15a889f90
Ruben Koster <rkoster@starkandwayne.com>
2019-10-01 10:47:20 +0200

    Sort public ips + add keys for easy access

--- bucc/molten_core_config.go
+++ bucc/molten_core_config.go
@@ -1,20 +1,26 @@
 package bucc
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/starkandwayne/molten-core/config"
+	"net"
+	"sort"
+	"strconv"
 )
 
 type moltenCoreConfig struct {
-	SingletonAZ string   `json:"az_singleton"`
-	OtherAZs    []string `json:"azs_other"`
-	AllAZs      []string `json:"azs"`
-	PublicIPs   []string `json:"public_ips"`
+	SingletonAZ string            `json:"az_singleton"`
+	OtherAZs    []string          `json:"azs_other"`
+	AllAZs      []string          `json:"azs"`
+	PublicIPs   map[string]string `json:"public_ips"`
 }
 
 func renderMoltenCoreConfig(confs *[]config.NodeConfig) (string, error) {
 	var mcconf moltenCoreConfig
+	mcconf.PublicIPs = make(map[string]string)
+	var publicIPs []net.IP
 
 	for _, conf := range *confs {
 		mcconf.AllAZs = append(mcconf.AllAZs, conf.Zone())
@@ -23,8 +29,16 @@ func renderMoltenCoreConfig(confs *[]config.NodeConfig) (string, error) {
 			mcconf.SingletonAZ = conf.Zone()
 		} else {
 			mcconf.OtherAZs = append(mcconf.OtherAZs, conf.Zone())
-			mcconf.PublicIPs = append(mcconf.PublicIPs, conf.PublicIP.String())
+			publicIPs = append(publicIPs, conf.PublicIP)
 		}
+	}
+
+	sort.Slice(publicIPs, func(i, j int) bool {
+		return bytes.Compare(publicIPs[i], publicIPs[j]) < 0
+	})
+
+	for i, ip := range publicIPs {
+		mcconf.PublicIPs[strconv.FormatInt(int64(i), 10)] = ip.String()
 	}
 
 	raw, err := json.Marshal(mcconf)
