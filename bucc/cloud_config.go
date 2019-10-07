fe882281190d4470af20132f10c175f549139bd1
Ruben Koster <rkoster@starkandwayne.com>
2019-10-07 13:20:13 +0200

    Generic cloud-config (remove CF specific stuff)

--- bucc/cloud_config.go
+++ bucc/cloud_config.go
@@ -21,7 +21,7 @@ const (
     }
   ],
   "compilation": {
-    "az": "z1",
+    "az": "z0",
     "network": "default",
     "reuse_compilation_vms": true,
     "vm_type": "default",
@@ -30,111 +30,10 @@ const (
   "disk_types": [
     {
       "disk_size": 1024,
-      "name": "1GB"
-    },
-    {
-      "disk_size": 5120,
-      "name": "5GB"
-    },
-    {
-      "disk_size": 10240,
-      "name": "10GB"
-    },
-    {
-      "disk_size": 100240,
-      "name": "100GB"
-    },
-    {
-      "disk_size": 1024,
       "name": "default"
     }
   ],
-  "vm_extensions": [
-    {
-      "name": "5GB_ephemeral_disk"
-    },
-    {
-      "name": "10GB_ephemeral_disk"
-    },
-    {
-      "name": "50GB_ephemeral_disk"
-    },
-    {
-      "name": "100GB_ephemeral_disk"
-    },
-    {
-      "name": "500GB_ephemeral_disk"
-    },
-    {
-      "name": "1TB_ephemeral_disk"
-    },
-    {
-      "name": "cf-router-network-properties",
-      "cloud_properties": {
-        "PortBindings": {
-          "80/tcp": [{ "HostPort": "80", "HostIp": "0.0.0.0" }],
-          "443/tcp": [{ "HostPort": "443", "HostIp": "0.0.0.0" }]
-        },
-        "ports": [
-          "80/tcp",
-          "443/tcp"
-        ]
-      }
-    },
-    {
-      "name": "diego-ssh-proxy-network-properties",
-      "cloud_properties": {
-        "PortBindings": {
-          "2222/tcp": [{ "HostPort": "2222", "HostIp": "0.0.0.0" }]
-        },
-        "ports": [
-          "2222/tcp"
-        ]
-      }
-    },
-    {
-      "name": "cf-tcp-router-network-properties",
-      "cloud_properties": {
-        "PortBindings": {
-          "1024/tcp": [{ "HostPort": "1024", "HostIp": "0.0.0.0" }],
-          "1025/tcp": [{ "HostPort": "1025", "HostIp": "0.0.0.0" }],
-          "1026/tcp": [{ "HostPort": "1026", "HostIp": "0.0.0.0" }],
-          "1027/tcp": [{ "HostPort": "1027", "HostIp": "0.0.0.0" }]
-        },
-        "ports": [
-          "1024/tcp",
-          "1025/tcp",
-          "1026/tcp",
-          "1027/tcp"
-        ]
-      }
-    }
-  ],
   "vm_types": [
-    {
-      "name": "minimal",
-      "cloud_properties": {
-        "RestartPolicy": {
-          "Name": "always"
-        }
-      }
-    },
-    {
-      "name": "small",
-      "cloud_properties": {
-        "RestartPolicy": {
-          "Name": "always"
-        }
-      }
-    },
-    {
-      "name": "small-highmem",
-      "cloud_properties": {
-        "RestartPolicy": {
-          "Name": "always"
-        }
-      }
-    },
     {
       "name": "default",
       "cloud_properties": {
